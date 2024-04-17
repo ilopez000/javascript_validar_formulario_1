8a016c92ab1077a6deeb5af5a1d85dd341470a2d
ilopez000 <ignacio.lopez.aylagas@gmail.com>
2024-04-17 16:50:34 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function validarFormulario() {
+    var nombre = document.getElementById('nombre').value;
+    var apellido = document.getElementById('apellido').value;
+    var email = document.getElementById('email').value;
+    var telefono = document.getElementById('telefono').value;
+
+
+    alert("Formulario enviado con éxito!");
+    // Aquí puedes agregar código para enviar los datos del formulario, como AJAX.
+}
