525465ad2a9f0ea4048dbefedbac03432e285a23
ilopez000 <ignacio.lopez.aylagas@gmail.com>
2024-04-18 16:09:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,29 @@ function validarFormulario() {
     let email = document.getElementById('email').value;
     let telefono = document.getElementById('telefono').value;
 
+    if(nombre.length<2){
+        alert("Nombre demasiado pequeño");
+        return;
+    }
 
+    if(apellido.length<2){
+        alert("Nombre demasiado pequeño");
+        return;
+    }
+
+    if( !email.includes('@') || !email.includes('.') ){
+        alert("El email no es correcto, revísalo");
+        return;
+    }
+
+    const expresion_regular=/^[0-9]*$/;
+    let solo_numeros=expresion_regular.test(telefono);
+
+    if(!expresion_regular || !telefono.length>=9){
+        alert("El teléfono no es correcto, revísalo");
+        return;
+    }
+    
     alert("Formulario enviado con éxito!");
     // Aquí puedes agregar código para enviar los datos del formulario, como AJAX.
 }
